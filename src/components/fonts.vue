b7bdac0bd3c12b9c4dec12288de88c8f9b6f87bb
Panutsaya Rujakom <mymint1598@gmail.com>
2020-09-04 13:57:00 +0700

    update

---
+++ src/components/fonts.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    
+    
+  </div>
+</template>
+
+<script>
+export default {
+  data: function () {
+    return {
+      counter: 0,
+      fsize:100,
+    };
+  },
+  props: {
+    lable: String,
+  },
+  methods: {
+    upFunction: function () {
+      this.fsize += 10;
+      console.log("fsize = " + this.fsize);
+    },
+    downFunction: function () {
+      this.fsize -= 10;
+      console.log(this.fsize);
+    },
+  },
+};
+</script> 
+<style>
+</style>
